f452417565fcc19ebb468ae34e5b096c9bf782b3
Christian Lopes <thelopesbrothers@gmail.com>
2023-10-02 20:43:45 -0400

    Commented code

--- scripts/test.py
+++ scripts/test.py
@@ -3,6 +3,7 @@ import json
 
 url = "https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20"
 
+#Parameters for restaurant selection
 parameters = {
     "location": "Stoughton",
     "latitude": "",
@@ -14,6 +15,7 @@ parameters = {
     "price": "",
 }
 
+#Iterate through parameters and append them to the end of the URL
 for param in parameters:
     if parameters[param] != "":
         url = url + "&" + param + "=" + parameters[param]
@@ -25,6 +27,8 @@ headers = {
     "Authorization": 'Bearer FoZr_sHH0IZ981_y58GdqRaLaKIzIc76HJuXhxbspj6oG8_Sd_Inx7WoFptaTjAitdmCpxLougxgzASnvpc9CgaRNhLsf0x3uWkkaUIDxlQHIGUC95e6JEqsJp8PZXYx'
 }
 
+#Fetch a response from the Yelp API
 response = requests.get(url, headers=headers)
 
+#Create a text file if it doesn't already exist and write the the fetched JSON to it. 
 with open('response.json', 'w') as f: json.dump(response.json(), f, indent="\t")
