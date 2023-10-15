e26d8769568ad3492ea996a578e382870a1a4364
Christian Lopes <thelopesbrothers@gmail.com>
2023-10-15 19:18:04 -0400

    Renamed the project folder. Created an app that for now displays a page with text.

--- Project/ResBuilder/ResBuilder/wsgi.py
+++ Project/ResBuilder/ResBuilder/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RezBuilder project.
+WSGI config for ResBuilder project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RezBuilder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ResBuilder.settings')
 
 application = get_wsgi_application()
