0e5a8f402eb8ca44f5ba25e6e42c7c9edfd3f6de
Christian Lopes <thelopesbrothers@gmail.com>
2023-10-16 16:06:22 -0400

    Moved API fetch script to myapp/script to keep things neat.
    Filters page for now only displays
    restaurants selected from the yelp.py
    script.

--- Project/ResBuilder/myapp/views.py
+++ Project/ResBuilder/myapp/views.py
@@ -1,44 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from myapp.scripts.yelp import response
 
 import requests
 import json
-
-
-
-url = "https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20"
-
-#Parameters for restaurant selection
-parameters = {
-    "location": "Stoughton",
-    "latitude": "",
-    "longitude": "",
-    "term": "",
-    "radius": "",
-    "categories": "",
-    "locale": "",
-    "price": "",
-}
-
-#Iterate through parameters and append them to the end of the URL
-for param in parameters:
-    if parameters[param] != "":
-        url = url + "&" + param + "=" + parameters[param]
-
-print(url,"/n")
-
-headers = {
-    "accept": "application/json",
-    "Authorization": 'Bearer FoZr_sHH0IZ981_y58GdqRaLaKIzIc76HJuXhxbspj6oG8_Sd_Inx7WoFptaTjAitdmCpxLougxgzASnvpc9CgaRNhLsf0x3uWkkaUIDxlQHIGUC95e6JEqsJp8PZXYx'
-}
-
-#Fetch a response from the Yelp API
-
-def response():
-    response_string = ""
-    response = requests.get(url, headers=headers).json()
-    for restaurant in response["businesses"]:
-       response_string += restaurant["name"] + "\n"
-    return response_string
 
 # Create your views here.
 
