0a0f627ff5390e388a507c6dd1d2a6d31693ce43
Michael Chambers <Michael.Chambers001@umb.edu>
2023-11-05 18:53:36 -0500

    TEST

--- Project/ResBuilder/myapp/urls.py
+++ Project/ResBuilder/myapp/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
 	path("landing_test/", views.landing_test, name="landing_test"),
 	path("filter/", views.filter, name="filter"),
     path("var_test/", views.var_test, name="var_test"),
-    path("Index1/", views.Index1, name="index")
 ]
